b034b6db920b79fe498957d4e3818bbbe64bc1a1
ujjaldas-11 <dasu82058@gmail.com>
2026-01-24 22:20:15 +0530

    solved daly temperature leetcode 739

---
+++ dailyTemperature.py
@@ -0,0 +1,14 @@
+# Input: temperatures = [73,74,75,71,69,72,76,73]
+# Output: [1,1,4,2,1,1,0,0]
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
+        return res   
