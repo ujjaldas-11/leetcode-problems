4acedf37769f8fff8574539e20dcfd10bb0e34fc
ujjaldas-11 <dasu82058@gmail.com>
2026-02-23 21:58:44 +0530

    solved leetcode 125

--- string/valid_pallindrome.py
+++ string/valid_pallindrome.py
@@ -1,7 +1,16 @@
+# leetcode 125 valid_pallindrome
+
 def isPallindrome(s):
     s = ''.join(c.lower() for c in s if c.isalnum())
     print(s)
     return s == s[::-1]
+
+
+
+
+
+    
+
 
 s = "Was it a car or a cat I saw?"
 
