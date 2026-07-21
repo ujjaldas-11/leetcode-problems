97874ad7147117669baf346e956b61f47d3e88a1
ujjaldas-11 <dasu82058@gmail.com>
2026-07-21 11:43:14 +0530

    solve leetcode 283

---
+++ two_pointer/move_zero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int left = 0;
+
+        for(int i = 0; i<nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[left]);
+                left++;
+            }
+        }
+
+
+    }
+};
+
